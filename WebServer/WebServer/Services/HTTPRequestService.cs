caaa361854b4d3603a3e19b7cb39df049f8308ac
Hodvogner Szilvia <szhodvogner@gmail.com>
2019-04-09 18:26:22 +0200

    Add HTTP Request client

---
+++ WebServer/WebServer/Services/HTTPRequestService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebServer.Services
+{
+    public class HTTPRequestService
+    {
+        private HttpClient httpClient;
+
+        public HTTPRequestService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public String getRequest(String ip)
+        {
+            var result = httpClient.GetAsync("https://www.google.com").Result;
+            return result.StatusCode.ToString();
+        }
+    }
+}
